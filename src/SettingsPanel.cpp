b16d95191a340d066611520c6af1fe9ba7fade64
Battousai90 <loukgilbert@gmail.com>
2025-08-13 01:15:26 +0700

    Features - adding rom scan - adding status bar - adding thumbnail - adding scan caching

--- src/SettingsPanel.cpp
+++ src/SettingsPanel.cpp
@@ -7,22 +7,40 @@
 SettingsPanel::~SettingsPanel() = default;
 
 SettingsPanel::SettingsPanel() : Box(Gtk::ORIENTATION_VERTICAL, 10) {
-    m_box.set_margin_start(10);
-    m_box.set_margin_end(10);
-    m_box.set_margin_top(10);
+    set_margin_start(10);
+    set_margin_end(10);
+    set_margin_top(10);
 
-    // --- ROMs ---
-    m_box.pack_start(m_label_roms, Gtk::PACK_SHRINK);
-    m_box.pack_start(m_entry_roms);
-    m_box.pack_start(m_button_browse_roms, Gtk::PACK_SHRINK);
+    // Create a grid for aligned layout
+    auto grid = Gtk::make_managed<Gtk::Grid>();
+    grid->set_column_spacing(8);   // Horizontal spacing between columns
+    grid->set_row_spacing(6);      // Vertical spacing between rows
+    grid->set_column_homogeneous(false);  // Allow different column widths
+
+    // --- ROMs Directory ---
+    grid->attach(m_label_roms, 0, 0, 1, 1);           // Label: Column 0
+    m_label_roms.set_halign(Gtk::ALIGN_START);         // Align label to the left
+    grid->attach(m_entry_roms, 1, 0, 1, 1);            // Entry: Column 1
+    m_entry_roms.set_hexpand(true);                    // Expand entry to fill space
+    grid->attach(m_button_browse_roms, 2, 0, 1, 1);    // Button: Column 2
+
+    m_button_browse_roms.set_image_from_icon_name("folder-open-symbolic", Gtk::ICON_SIZE_BUTTON);
+    m_button_browse_roms.set_always_show_image(true);
+    m_button_browse_roms.set_label("Browse");
     m_button_browse_roms.signal_clicked().connect([this] {
         on_folder_clicked(&m_entry_roms);
     });
 
-    // --- DAT ---
-    m_box.pack_start(m_label_dat, Gtk::PACK_SHRINK);
-    m_box.pack_start(m_entry_dat);
-    m_box.pack_start(m_button_browse_dat, Gtk::PACK_SHRINK);
+    // --- DAT File ---
+    grid->attach(m_label_dat, 0, 1, 1, 1);
+    m_label_dat.set_halign(Gtk::ALIGN_START);
+    grid->attach(m_entry_dat, 1, 1, 1, 1);
+    m_entry_dat.set_hexpand(true);
+    grid->attach(m_button_browse_dat, 2, 1, 1, 1);
+
+    m_button_browse_dat.set_image_from_icon_name("document-open-symbolic", Gtk::ICON_SIZE_BUTTON);
+    m_button_browse_dat.set_always_show_image(true);
+    m_button_browse_dat.set_label("Select");
     m_button_browse_dat.signal_clicked().connect([this] {
         auto dialog = Gtk::FileChooserDialog("Select DAT File", Gtk::FILE_CHOOSER_ACTION_OPEN);
         dialog.add_button("Cancel", Gtk::RESPONSE_CANCEL);
@@ -40,17 +58,29 @@ SettingsPanel::SettingsPanel() : Box(Gtk::ORIENTATION_VERTICAL, 10) {
     });
 
     // --- Thumbnails ---
-    m_box.pack_start(m_label_thumbs, Gtk::PACK_SHRINK);
-    m_box.pack_start(m_entry_thumbs);
-    m_box.pack_start(m_button_browse_thumbs, Gtk::PACK_SHRINK);
+    grid->attach(m_label_thumbs, 0, 2, 1, 1);
+    m_label_thumbs.set_halign(Gtk::ALIGN_START);
+    grid->attach(m_entry_thumbs, 1, 2, 1, 1);
+    m_entry_thumbs.set_hexpand(true);
+    grid->attach(m_button_browse_thumbs, 2, 2, 1, 1);
+
+    m_button_browse_thumbs.set_image_from_icon_name("folder-open-symbolic", Gtk::ICON_SIZE_BUTTON);
+    m_button_browse_thumbs.set_always_show_image(true);
+    m_button_browse_thumbs.set_label("Select");
     m_button_browse_thumbs.signal_clicked().connect([this] {
         on_folder_clicked(&m_entry_thumbs);
     });
 
-        // --- FBNeo Executable ---
-    m_box.pack_start(m_label_fbneo, Gtk::PACK_SHRINK);
-    m_box.pack_start(m_entry_fbneo);
-    m_box.pack_start(m_button_browse_fbneo, Gtk::PACK_SHRINK);
+    // --- FBNeo Executable ---
+    grid->attach(m_label_fbneo, 0, 3, 1, 1);
+    m_label_fbneo.set_halign(Gtk::ALIGN_START);
+    grid->attach(m_entry_fbneo, 1, 3, 1, 1);
+    m_entry_fbneo.set_hexpand(true);
+    grid->attach(m_button_browse_fbneo, 2, 3, 1, 1);
+
+    m_button_browse_fbneo.set_image_from_icon_name("application-x-executable-symbolic", Gtk::ICON_SIZE_BUTTON);
+    m_button_browse_fbneo.set_always_show_image(true);
+    m_button_browse_fbneo.set_label("Select");
     m_button_browse_fbneo.signal_clicked().connect([this] {
         auto dialog = Gtk::FileChooserDialog("Select FBNeo Executable", Gtk::FILE_CHOOSER_ACTION_OPEN);
         dialog.add_button("Cancel", Gtk::RESPONSE_CANCEL);
@@ -70,8 +100,26 @@ SettingsPanel::SettingsPanel() : Box(Gtk::ORIENTATION_VERTICAL, 10) {
         }
     });
 
-    add(m_box);
-    show_all_children();
+    // Add the grid to the main container
+    pack_start(*grid, Gtk::PACK_SHRINK);
+
+    // --- Save & Cancel buttons ---
+    Gtk::HBox button_box;
+    button_box.pack_end(m_button_save, Gtk::PACK_SHRINK, 5);
+    button_box.pack_end(m_button_cancel, Gtk::PACK_SHRINK, 5);
+
+    m_button_save.set_image_from_icon_name("document-save-symbolic", Gtk::ICON_SIZE_BUTTON);
+    m_button_save.set_always_show_image(true);
+    m_button_save.set_label("Save");
+
+    m_button_cancel.set_image_from_icon_name("window-close-symbolic", Gtk::ICON_SIZE_BUTTON);
+    m_button_cancel.set_always_show_image(true);
+    m_button_cancel.set_label("Cancel");
+
+    pack_start(button_box, Gtk::PACK_SHRINK);
+
+    // Show all widgets
+    show_all();  // Must be called at the end
 }
 
 void SettingsPanel::on_folder_clicked(Gtk::Entry* entry) {
@@ -93,13 +141,13 @@ void SettingsPanel::on_folder_clicked(Gtk::Entry* entry) {
 std::string SettingsPanel::get_roms_path() const { return m_entry_roms.get_text(); }
 std::string SettingsPanel::get_dat_path() const { return m_entry_dat.get_text(); }
 std::string SettingsPanel::get_thumbnails_path() const { return m_entry_thumbs.get_text(); }
-std::string SettingsPanel::get_fbneo_executable() const { return m_entry_fbneo.get_text();}
+std::string SettingsPanel::get_fbneo_executable() const { return m_entry_fbneo.get_text(); }
 
 // --- Setters ---
 void SettingsPanel::set_roms_path(const std::string& path) { m_entry_roms.set_text(path); }
 void SettingsPanel::set_dat_path(const std::string& path) { m_entry_dat.set_text(path); }
 void SettingsPanel::set_thumbnails_path(const std::string& path) { m_entry_thumbs.set_text(path); }
-void SettingsPanel::set_fbneo_executable(const std::string& path) { m_entry_fbneo.set_text(path);}
+void SettingsPanel::set_fbneo_executable(const std::string& path) { m_entry_fbneo.set_text(path); }
 
 // --- Load / Save ---
 bool SettingsPanel::load_from_file(const std::string& filename) {
@@ -132,6 +180,6 @@ bool SettingsPanel::save_to_file(const std::string& filename) {
     std::ofstream file(filename);
     if (!file.is_open()) return false;
 
-    file << j.dump(4); // Formaté avec 4 espaces
+    file << j.dump(4); // Pretty print with 4 spaces
     return true;
 }
